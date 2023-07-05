79332326474c98d8aa12aefb2b6fd70f085783c0
javierguti <gutierrezjavieralejandro@gmail.com>
2023-07-04 23:06:34 -0300

    ejercicios 4

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -26,17 +26,19 @@ function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
 
-   
+   return  Math.round(num)
 }
 
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
    // Tu código:
+   return  Math.ceil(num)
 }
 
 function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
+   return Math.random()
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
